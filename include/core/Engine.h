821535cfa668d50e9b495aebbfb1486ebfe42d61
Tanner Mickelson <tanner@icicle-games.com>
2015-01-25 12:21:41 -0800

    Added window event callbacks

--- include/core/Engine.h
+++ include/core/Engine.h
@@ -1,15 +1,38 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 #include "core/Uncopyable.h"
+#include <SFML/Window.hpp>
+#include <vector>
+#include <memory>
 
 namespace ice
 {
 	namespace core
 	{
+		class WindowEventCallback
+		{
+		public:
+		
+			virtual ~WindowEventCallback(){}
+			
+			virtual void onWindowEvent(const sf::Event& evt){}
+		};
+	
 		class Engine : public Uncopyable
 		{
 		public:
-			Engine();
+		
+			Engine(sf::Window* window);
+			
+			void startGame();
+			
+			sf::Window* getWindow(){return m_window;}
+			
+		protected:
+			
+			std::unique_ptr<sf::Window> m_window;
+			
+			std::vector<WindowEventCallback*> m_windowCallbacks; 
 		};
 	}
 }
--- source/core/Engine.cpp
+++ source/core/Engine.cpp
@@ -9,6 +9,30 @@ using namespace core;
 
 using namespace std;
 
-Engine::Engine() {
+Engine::Engine(sf::Window* window)
+	:m_window(window)
+{
 	
 }
+
+void Engine::startGame()
+{
+	while (m_window->isOpen())
+	{
+		//Handle window events
+		sf::Event evt;
+		while (m_window->pollEvent(evt))
+		{
+			//Send window event to all callbacks
+			for (unsigned int i = 0; i < m_windowCallbacks.size(); ++i)
+			{
+				m_windowCallbacks[i]->onWindowEvent(evt);
+			}
+
+			if (evt.type == sf::Event::Closed)
+			{
+				m_window->close();
+			}
+		}
+
+	}
