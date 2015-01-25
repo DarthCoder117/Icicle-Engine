c219a99c6dd30ce8f9be449a7b0247b1438abb6c
Tanner Mickelson <tanner@icicle-games.com>
2015-01-25 09:10:36 -0800

    Added empty engine class and Uncopyable class.

---
+++ include/core/Engine.h
@@ -0,0 +1,18 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+#include "core/Uncopyable.h"
+
+namespace ice
+{
+	namespace core
+	{
+		class Engine : public Uncopyable
+		{
+		public:
+			
+			
+		};
+	}
+}
+
+#endif
