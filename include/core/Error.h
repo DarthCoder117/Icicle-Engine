132458ccec4a7467928f723c914fae93f7edc75c
Tanner Mickelson <tanner@icicle-games.com>
2015-01-28 07:28:05 -0800

    Added Error class.

---
+++ include/core/Error.h
@@ -0,0 +1,30 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+namespace ice
+{
+	namespace core
+	{
+		enum ERROR_CODE
+		{
+			EC_OUT_OF_MEMORY
+		};
+
+		class Error
+		{
+		public:
+
+			Error(ERROR_CODE errorCode);
+
+			ERROR_CODE getCode();
+
+			const char* getMessage();
+
+		private:
+
+			ERROR_CODE m_errorCode;
+		};
+	}
+}
+
+#endif
